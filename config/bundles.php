2aaeca0c00397eefff03c76368bbe162e604bec4
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-28 05:28:04 +0200

    Installed EasyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
