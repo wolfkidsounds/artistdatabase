15c0521ed1d23bf64eb8618048c4317be79e0ff2
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-09-29 03:33:58 +0200

    Composer Update & Install Doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
