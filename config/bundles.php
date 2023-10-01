76af95907b1c5d94e933477f7df15bf506099a7c
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-01 09:36:12 +0200

    Adding Rate Limiter & Password Hasher

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
