dbee47c57b0dbf9cca7d10cda759fea835f5bc79
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-02 00:28:25 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,15 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+import 'bootstrap';
+
 import './styles/app.css';
+
+document.addEventListener('turbo:load', function (e) {
+    // this enables bootstrap tooltips globally
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    });
+});
