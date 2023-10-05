b95e9699314ce23a139ae00cdf082c2384323638
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-05 09:05:37 +0200

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,7 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('index.html.twig', [
+        return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
