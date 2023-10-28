d0b640a4463d3c3704028584662768335529aa33
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-10-29 01:47:56 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,35 +6,35 @@ call git status | findstr /C:"composer.lock" /C:"composer.json" /C:"symfony.lock
 
 if %errorlevel%==0 (
     echo ======================================================
-    echo Es wurden Änderungen in Composer-, Symfony- oder NPM-Dateien gefunden.
+    echo Es wurden Aenderungen in Composer-, Symfony- oder NPM-Dateien gefunden.
     echo Bitte bestätige diese Änderungen, damit du fortfahren kannst.
     echo ======================================================
     pause
     exit /b
 ) else (
     echo ======================================================
-    echo Es gibt keine aktuellen Änderungen in Composer-, Symfony- oder NPM-Dateien.
+    echo Es gibt keine aktuellen Aenderungen in Composer-, Symfony- oder NPM-Dateien.
     echo Das Skript wird weiter ausgeführt.
     echo ======================================================
 )
 
 echo ======================================================
-echo Änderungen aus dem GitHub-Repository abrufen.
+echo Aenderungen aus dem GitHub-Repository abrufen.
 echo ======================================================
 call git pull
 
 echo ======================================================
-echo Speichern von aktuellen Änderungen
+echo Speichern von aktuellen Aenderungen
 echo ======================================================
 call git stash save "Aktuelle Änderungen speichern"
 
 echo ======================================================
-echo Installieren von Composer-Abhängigkeiten
+echo Installieren von Composer-Abhaengigkeiten
 echo ======================================================
 call composer install
 
 echo ======================================================
-echo Installieren von Node.js-Abhängigkeiten
+echo Installieren von Node.js-Abhaengigkeiten
 echo ======================================================
 call npm install
 
@@ -45,13 +45,13 @@ call composer update
 call npm update
 
 echo ======================================================
-echo Hinzufügen, Übertragen und Veröffentlichen von Änderungen auf GitHub
+echo Hinzufuegen, Uebertragen und Veroeffentlichen von Aenderungen auf GitHub
 echo ======================================================
 call git add .
 call git commit -m "Update dependencies"
 call git push
 
 echo ======================================================
-echo Gespeicherte Änderungen wiederherstellen.
+echo Gespeicherte Aenderungen wiederherstellen.
 echo ======================================================
 call git stash pop
